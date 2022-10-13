686218bf12f280501f70c8c6cde266417d08d15f
Matthew <MatthewGebara@gmail.com>
2022-10-13 14:45:55 -0400

    Updated

--- script.js
+++ script.js
@@ -3,3 +3,26 @@ let question = { //sample question
     alternatives: ['Github', 'Your PC', 'Stack Overflow', 'Megaupload'],
     correctAnswer: 1
   };
+
+function showQuestion(q){
+
+}
+
+showQuestion(question);
+
+let btn = document.getElementById('btn');
+btn.addEventListener('click', function() {
+  console.log('Clicked!');
+});
+
+var countDown = 15;
+function timer() {
+  var interval = setInterval(function() {
+    secondsleft--;
+    DocumentTimeline.textcontent = secondsleft + " seconds left.";
+
+    if(secondsleft === 0) {
+      failstate = true;
+    }
+  }
+}
