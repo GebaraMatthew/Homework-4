a292da0276fa30e4470e43b3c2431b196a94cc32
Matthew <MatthewGebara@gmail.com>
2022-10-13 14:35:54 -0400

    Placeholder code

---
+++ script.js
@@ -0,0 +1,5 @@
+let question = { //sample question
+    title: 'Where do you save your repositories?',
+    alternatives: ['Github', 'Your PC', 'Stack Overflow', 'Megaupload'],
+    correctAnswer: 1
+  };
